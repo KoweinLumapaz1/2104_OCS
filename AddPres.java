4d7b61596a53af203ab675a7410928e6ecd60cf6
JellyAcexx <ata.liraangela.r@gmail.com>
2024-11-15 21:32:52 +0800

    FOURTH COMMIT

--- AddPres.java
+++ AddPres.java
@@ -5,6 +5,7 @@
 package prescription;
 
 import java.sql.*;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -47,27 +48,39 @@ public class AddPres extends javax.swing.JFrame {
         jLabel6 = new javax.swing.JLabel();
         Textcontact = new javax.swing.JTextField();
         addButton = new javax.swing.JButton();
-        updateButton = new javax.swing.JButton();
         Fradio = new javax.swing.JRadioButton();
         Mradio = new javax.swing.JRadioButton();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        Textdate = new javax.swing.JTextField();
+        Textreserved = new javax.swing.JTextField();
+        Textdiag = new javax.swing.JTextField();
+        Texteye = new javax.swing.JTextField();
+        Textframe = new javax.swing.JTextField();
+        backB = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setForeground(new java.awt.Color(255, 255, 255));
+        setPreferredSize(new java.awt.Dimension(665, 400));
+        setResizable(false);
 
         jPanel1.setBackground(java.awt.SystemColor.inactiveCaption);
         jPanel1.setFont(new java.awt.Font("Serif", 0, 12)); // NOI18N
 
         jLabel1.setFont(new java.awt.Font("Serif", 1, 18)); // NOI18N
-        jLabel1.setText("Update Form");
+        jLabel1.setText("LJK Optical Clinic Add Form");
 
         jLabel2.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
-        jLabel2.setText("Name:");
+        jLabel2.setText("PatientName:");
 
         jLabel3.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
-        jLabel3.setText("Age:\n");
+        jLabel3.setText("PatientAge: ");
 
         jLabel4.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
-        jLabel4.setText("Address ");
+        jLabel4.setText("PatientAddress ");
 
         Textname.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -88,10 +101,10 @@ public class AddPres extends javax.swing.JFrame {
         });
 
         jLabel5.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
-        jLabel5.setText("Sex:");
+        jLabel5.setText("PatientSex:");
 
         jLabel6.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
-        jLabel6.setText("Contact:");
+        jLabel6.setText("PatientContact:");
 
         Textcontact.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -101,16 +114,12 @@ public class AddPres extends javax.swing.JFrame {
 
         addButton.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
         addButton.setText("Add New\n");
-        addButton.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, null, new java.awt.Color(204, 204, 204), null, null));
+        addButton.setBorder(null);
         addButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 addButtonActionPerformed(evt);
             }
         });
-
-        updateButton.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
-        updateButton.setText("Update");
-        updateButton.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, null, new java.awt.Color(204, 204, 204), null, null));
 
         Fradio.setFont(new java.awt.Font("Serif", 0, 12)); // NOI18N
         Fradio.setText("F\n");
@@ -128,6 +137,29 @@ public class AddPres extends javax.swing.JFrame {
             }
         });
 
+        jLabel7.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
+        jLabel7.setText("DateAppointed:");
+
+        jLabel8.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
+        jLabel8.setText("ReservedDate:");
+
+        jLabel9.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
+        jLabel9.setText("Diagnosis:");
+
+        jLabel10.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
+        jLabel10.setText("EyeGradeLevel:");
+
+        jLabel11.setFont(new java.awt.Font("Serif", 1, 14)); // NOI18N
+        jLabel11.setText("GlassFrame:");
+
+        backB.setFont(new java.awt.Font("Serif", 0, 14)); // NOI18N
+        backB.setText("Back");
+        backB.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                backBActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -135,18 +167,22 @@ public class AddPres extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(150, 150, 150)
+                        .addGap(196, 196, 196)
                         .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(26, 26, 26)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(backB, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jLabel3)
                                     .addComponent(jLabel2)
                                     .addComponent(jLabel4)
                                     .addComponent(jLabel5)
-                                    .addComponent(jLabel6))
+                                    .addComponent(jLabel6)
+                                    .addComponent(addButton, javax.swing.GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(Textname)
@@ -157,44 +193,69 @@ public class AddPres extends javax.swing.JFrame {
                                         .addComponent(Fradio)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 60, Short.MAX_VALUE)
                                         .addComponent(Mradio)
-                                        .addGap(10, 10, 10))))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(addButton)
-                                .addGap(18, 18, 18)
-                                .addComponent(updateButton)))))
-                .addContainerGap(147, Short.MAX_VALUE))
+                                        .addGap(10, 10, 10)))
+                                .addGap(52, 52, 52)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel11)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jLabel7)
+                                            .addComponent(jLabel8)
+                                            .addComponent(jLabel9))
+                                        .addGap(5, 5, 5)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(Textdate)
+                                            .addComponent(Textreserved)
+                                            .addComponent(Textdiag, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jLabel10)
+                                        .addGap(5, 5, 5)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(Texteye, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addComponent(Textframe, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE))))))))
+                .addContainerGap(62, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(14, 14, 14)
                 .addComponent(jLabel1)
-                .addGap(18, 18, 18)
+                .addGap(26, 26, 26)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(Textname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(Textname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel7)
+                    .addComponent(Textdate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(6, 6, 6)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel3)
-                    .addComponent(Textage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(Textage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel8)
+                    .addComponent(Textreserved, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(6, 6, 6)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(TextAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(TextAddress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel9)
+                    .addComponent(Textdiag, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(6, 6, 6)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(Fradio)
-                    .addComponent(Mradio))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(Mradio)
+                    .addComponent(jLabel10)
+                    .addComponent(Texteye, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(6, 6, 6)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(Textcontact, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(Textcontact, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel11)
+                    .addComponent(Textframe, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(addButton)
-                    .addComponent(updateButton))
-                .addContainerGap(49, Short.MAX_VALUE))
+                .addComponent(addButton, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(backB, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(112, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -216,34 +277,28 @@ public class AddPres extends javax.swing.JFrame {
             String name = Textname.getText();
             String age = Textage.getText();
             String address = TextAddress.getText();
-            String sex = Fradio.isSelected() ? "F" : "M"; // Selects "F" if Fradio is selected, otherwise "M".
+            String sex = Fradio.isSelected() ? "F" : "M";
             String contact = Textcontact.getText();
+            String dateAppointed = Textdate.getText();
+            String reservedDate = Textreserved.getText();
+            String diagnosis = Textdiag.getText();
+            String eyeGradeLevel = Texteye.getText();
+            String glassFrame = Textframe.getText();
+            
+             
 
-
-            pst = con.prepareStatement("INSERT INTO prescriptions(PatientName, PatientAge, PatientAddress, PatientSex, PatientContact, PatientName, DateAppointed,ReservedDate, Diagnosis, EyeGradeLevel, GlassFrame)VALUES(?,?,?,?,?,?,?,?,?,?)");
-            String PatientName = null;
-            String PatientAge = null;
-            String PatientAddress = null;
-            String PatientSex = null;
-            String PatientContact = null;
-            String DateAppointed = null;
-            String ReservedDate = null;
-            String Diagnosis = null;
-            String EyeGradeLevel = null;
-            String GlassFrame = null;
+            pst = con.prepareStatement("INSERT INTO prescriptions(PatientName, PatientAge, PatientAddress, PatientSex, PatientContact, DateAppointed, ReservedDate, Diagnosis, EyeGradeLevel, GlassFrame)VALUES(?,?,?,?,?,?,?,?,?,?)");
             
-            
-            
-            pst.setString(1,PatientName);
-            pst.setString(2,PatientAge);
-            pst.setString(3,PatientAddress);
-            pst.setString(4,PatientSex);
-            pst.setString(5,PatientContact);
-            pst.setString(6,DateAppointed);
-            pst.setString(7,ReservedDate);
-            pst.setString(8,Diagnosis);
-            pst.setString(9,EyeGradeLevel);
-            pst.setString(10,GlassFrame);
+            pst.setString(1,name);
+            pst.setString(2,age);
+            pst.setString(3,address);
+            pst.setString(4,sex);
+            pst.setString(5,contact);
+            pst.setString(6,dateAppointed);
+            pst.setString(7,reservedDate);
+            pst.setString(8,diagnosis);
+            pst.setString(9,eyeGradeLevel);
+            pst.setString(10,glassFrame);
             
             int k = pst.executeUpdate();
             
@@ -255,6 +310,15 @@ public class AddPres extends javax.swing.JFrame {
                 Fradio.setSelected(false);
                 Mradio.setSelected(false);
                 Textcontact.setText("");
+                Textdate.setText("");
+                Textreserved.setText("");
+                Textdiag.setText("");
+                Texteye.setText("");
+                Textframe.setText("");
+                
+                
+                Prescription01 Update = new Prescription01();
+                Update.DisplayData();
             }else{
                 JOptionPane.showMessageDialog(this,"Record failed to saved!");
             }
@@ -263,8 +327,7 @@ public class AddPres extends javax.swing.JFrame {
         } catch (SQLException ex) {
             Logger.getLogger(AddPres.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+        this.dispose();       
     }//GEN-LAST:event_addButtonActionPerformed
 
     private void TextnameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextnameActionPerformed
@@ -290,6 +353,13 @@ public class AddPres extends javax.swing.JFrame {
     private void TextcontactActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextcontactActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_TextcontactActionPerformed
+
+    private void backBActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backBActionPerformed
+        this.dispose(); 
+  
+    Prescription01 mainPrescription = new Prescription01();
+    mainPrescription.setVisible(true);
+    }//GEN-LAST:event_backBActionPerformed
 
     /**
      * @param args the command line arguments
@@ -332,15 +402,25 @@ public class AddPres extends javax.swing.JFrame {
     private javax.swing.JTextField TextAddress;
     private javax.swing.JTextField Textage;
     private javax.swing.JTextField Textcontact;
+    private javax.swing.JTextField Textdate;
+    private javax.swing.JTextField Textdiag;
+    private javax.swing.JTextField Texteye;
+    private javax.swing.JTextField Textframe;
     private javax.swing.JTextField Textname;
+    private javax.swing.JTextField Textreserved;
     private javax.swing.JButton addButton;
+    private javax.swing.JButton backB;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton updateButton;
     // End of variables declaration//GEN-END:variables
 }
